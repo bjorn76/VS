5d3484650a1a58dccc5581da63105bd000c2e919
bjorn76 <bjorn.k.j@gmail.com>
2017-03-06 23:10:29 +0100

    b2

---
+++ VS2015-ThreadSafeDemo/BackGroundWorkerB2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ThreadSafe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bgw1 = new System.ComponentModel.BackgroundWorker();
+            this.setTextBackgroundWorkerBtn = new System.Windows.Forms.Button();
+            this.bgw2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 263);
+            this.textBox1.TabIndex = 1;
+            // 
+            // bgw1
+            // 
+            this.bgw1.WorkerReportsProgress = true;
+            this.bgw1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw1_DoWork);
+            this.bgw1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw1_ProgressChanged);
+            this.bgw1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw1_RunWorkerCompleted);
+            // 
+            // setTextBackgroundWorkerBtn
+            // 
+            this.setTextBackgroundWorkerBtn.Location = new System.Drawing.Point(12, 12);
+            this.setTextBackgroundWorkerBtn.Name = "setTextBackgroundWorkerBtn";
+            this.setTextBackgroundWorkerBtn.Size = new System.Drawing.Size(186, 23);
+            this.setTextBackgroundWorkerBtn.TabIndex = 3;
+            this.setTextBackgroundWorkerBtn.Text = "setTextBackgroundWorkerBtn";
+            this.setTextBackgroundWorkerBtn.UseVisualStyleBackColor = true;
+            this.setTextBackgroundWorkerBtn.Click += new System.EventHandler(this.setTextBackgroundWorkerBtn_Click);
+            // 
+            // bgw2
+            // 
+            this.bgw2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw2_DoWork);
+            this.bgw2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw2_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 316);
+            this.Controls.Add(this.setTextBackgroundWorkerBtn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker bgw1;
+        private System.Windows.Forms.Button setTextBackgroundWorkerBtn;
+        private System.ComponentModel.BackgroundWorker bgw2;
+    }
+}
+
---
+++ VS2015-ThreadSafeDemo/BackGroundWorkerB2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace ThreadSafe
+{
+    public partial class Form1 : Form
+    {
+        // This BackgroundWorker is used to demonstrate the 
+        // preferred way of performing asynchronous operations.
+
+
+        private string rText="", rText2="";
+
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+ 
+            private void setTextBackgroundWorkerBtn_Click(
+                object sender,
+                EventArgs e)
+            {
+                this.bgw1.RunWorkerAsync();
+                this.bgw2.RunWorkerAsync();
+
+        }
+
+        // This event handler sets the Text property of the TextBox
+        // control. It is called on the thread that created the 
+        // TextBox control, so the call is thread-safe.
+        //
+        // BackgroundWorker is the preferred way to perform asynchronous
+        // operations.
+
+        private void bgw1_DoWork(object sender, DoWorkEventArgs e)
+        {
+           
+            for (int i= 0; i <= 4; i++)
+            {
+                rText +=  i.ToString() + Environment.NewLine;
+                bgw1.ReportProgress((i+1)*25);
+            }
+            rText += "bgw1_dowork" + Environment.NewLine; ;
+
+
+        }
+        private void bgw1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+            rText += "bgw1 progress: "+ e.ProgressPercentage.ToString() + Environment.NewLine;
+
+
+        }
+
+
+
+        private void bgw1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+            {
+            this.textBox1.AppendText(rText);
+            rText = "";
+            this.textBox1.AppendText( "#SafelyByBackgroundWorkerCompleted."+ Environment.NewLine);
+            
+        }
+
+ 
+
+        private void bgw2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            rText2 += "#BGW2" + Environment.NewLine;
+        }
+
+  
+
+        private void bgw2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.textBox1.AppendText(rText2);
+            rText2 = "";
+            this.textBox1.AppendText("#bgw_Completed." + Environment.NewLine);
+
+        }
+
+
+    }
+
+}
+
