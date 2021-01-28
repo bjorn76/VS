ba354309c67efae422972b9751a53eb861e24d20
bjorn76 <bjorn.k.j@gmail.com>
2021-01-28 20:14:34 +0100

    B2

--- VS2019-Excel_add-in_LDAP/Excel_add-in_LDAP/ExcelFunctionCalls.cs
+++ VS2019-Excel_add-in_LDAP/Excel_add-in_LDAP/ExcelFunctionCalls.cs
@@ -5,14 +5,10 @@ using System.Linq;
 
 public static class ExcelFunctionCalls
 {
-    [ExcelFunction(Description = "Last week example UDF Function")]
-    public static DateTime GetLastWeek()
-    {
-        return DateTime.Today - TimeSpan.FromDays(7);
-    }
+
 
     [ExcelFunction(Description = "Reverse string example UDF Function")]
-    public static string ReverseString([ExcelArgument(Name = "input_string",
+    public static string B2_ReverseString([ExcelArgument(Name = "input_string",
                                                       Description = "String to reverse")]
                                         string input_string)
 
@@ -27,90 +23,133 @@ public static class ExcelFunctionCalls
         return reversestring;
     }
 
-    [ExcelFunction(Description = "Distinguished Name string of user in a UDF Function")]
-    public static string B2_UserDN([ExcelArgument(Name = "user_id",
-                                                      Description = "user id as String to look up")]
-                                        string user_id)
 
-    {
-        string DNstring = "";
+    /*
+     https://docs.microsoft.com/en-us/previous-versions/windows/desktop/ldap/using-the-ldap-api-in-a-client-application
 
-        var searcher = new DirectorySearcher("LDAP://" + user_id.Split('\\').First().ToLower())
-        {
-            Filter = "(&(ObjectClass=person)(sAMAccountName=" + user_id.Split('\\').Last().ToLower() + "))"
-        };
-
-        var result = searcher.FindOne();
-            if (result == null)
-                return string.Empty;
-
-
-            DNstring= result.Properties["distinguishedName"][0].ToString();
-        return DNstring;
-    }
-
-
-    [ExcelFunction(Description = "email of user in a UDF Function")]
-    public static string B2_User2Mail([ExcelArgument(Name = "user_id",
-                                                      Description = "user id as String to look up")]
-                                        string user_id)
-
-    {
-
-        var searcher = new DirectorySearcher("LDAP://" + user_id.Split('\\').First().ToLower())
-        {
-            Filter = "(&(ObjectClass=person)(sAMAccountName=" + user_id.Split('\\').Last().ToLower() + "))"
-        };
-
-        var result = searcher.FindOne();
-        if (result == null)
-            return string.Empty;
+     
+     Names and emails
+     =========================
+     name =
+     cn = Administrator
+     sn = Kwiatek (Last name)
+     displayName = is abrivated Disp 
+     givenName = Piotr (First name)
+     title = .NET Developer
+  
+     
+     IT ID's 
+     ========================
+     sAMAccountName =
+     mail =
+     distinguishedName = is abrivated DN
+     c = PL (Country Code)
+     
+     description = 
+     objectCategory = CN=Person,CN=Schema,CN=Configuration,DC=helpdesk,DC=wat,DC=edu
+     countryCode = 616
+    
+     whenCreated = 2012-11-23 06:09:28
+     whenChanged = 2013-02-23 13:24:41
+     wWWHomePage = http://www.piotr.kwiatek.org
 
 
-        return result.Properties["mail"][0].ToString();
 
-    }
+     Street, City and Country
+     ========================
+     not working - st = Mazowieckie (Voivodeship)
+     l = Warszawa (City)
+     postalCode = 00-000
+     postOfficeBox = Warszawa Ursynów
+     physicalDeliveryOfficeName = Wojskowa Akademia Techniczna
+     co = Poland
+     company = HELPDESK
+     streetAddress = Kaliskiego 2
+   
 
-    //userPrincipalName
-    [ExcelFunction(Description = "email of user in a UDF Function")]
-    public static string B2_mail2user([ExcelArgument(Name = "user_mail",
-                                                      Description = "user mail as String to look up")]
-                                        string user_mail)
-
-    {
-
-        var searcher = new DirectorySearcher("LDAP://" + user_mail.Split('\\').First().ToLower())
-        {
-            
-            Filter = "(&(objectCategory=User)(ObjectClass=person)(mail=" + user_mail.Split('\\').Last().ToLower() + "*))"
-        };
-
-        var result = searcher.FindOne();
-        if (result == null)
-            return string.Empty;
+     ************************************************************** 
+     B2_User2DN
+     B2_User2Mail B2_mail2user
 
 
-        //return result.Properties["CN"][0].ToString();
-        //return result.Properties["userPrincipalName"][0].ToString();  //also email
-        return result.Properties["sAMAccountName"][0].ToString();
+        Todo:
+        Site
+        First name
+        Last name
+        Display name
+        
+        Could do:
+        User Principal Name
+        Initials
+        Business Unit Code
+        Fully qualified domain name of the object
         
 
 
+     */
 
+    [ExcelFunction(Description = " x of user.  (C# UDF Function")]
+    public static string B2_User2x([ExcelArgument(Name = "user_id", Description = "user id (eg johanb01)")] string user_id)
+    {
+     
+
+        var searcher = new DirectorySearcher("LDAP://" + user_id.Split('\\').First().ToLower())
+        {
+            Filter = "(&(ObjectClass=person)(sAMAccountName=" + user_id.Split('\\').Last().ToLower() + "))"
+        };
+
+        var result = searcher.FindOne();
+        if (result == null)
+            return string.Empty;
+
+        string str01 = ""; //Just to get a break point
+
+        // Names and user
+        str01 = result.Properties["sAMAccountName"][0].ToString(); ; //sAMAccountName
+        string str02 = result.Properties["distinguishedName"][0].ToString(); // Long string...
+        string str03 = result.Properties["mail"][0].ToString(); // se also userPrincipalName
+        string str04 = result.Properties["userPrincipalName"][0].ToString();  //also mail
+
+        string str05 =  result.Properties["CN"][0].ToString();        //Full name
+        string str06 = result.Properties["givenName"][0].ToString();  //First name sn
+        string str07 = result.Properties["sn"][0].ToString();         //Sir Name aka last name
+
+        string str10 = result.Properties["title"][0].ToString();
+        string str11 = result.Properties["description"][0].ToString();
+
+        //Address and Contry and
+        string str51 = result.Properties["c"][0].ToString();  //e.g. SE
+        string str52 = result.Properties["co"][0].ToString();  //e.g.Sweden
+        string str53 = result.Properties["l"][0].ToString();  //City e.g Gothenburg
+
+
+        //str = result.Properties[""][0].ToString(); 
+
+        //DONT WORK
+        // = result.Properties["site"][0].ToString();  
+        //= result.Properties["st"][0].ToString();  
+        // = result.Properties["postOfficeBox "][0].ToString();
+
+        //DONT WORK
+
+        //Location
+        //https://ldap.com/ldap-filters/
+
+
+        return str53;
     }
 
-    //userPrincipalName
-    [ExcelFunction(Description = "email of user in a UDF Function")]
-    public static string B2_mail2mail([ExcelArgument(Name = "user_mail",
-                                                      Description = "start of user mail as String to look up")]
-                                        string user_mail)
+
+    
+    private static string User2(string user_id,
+                                string dirParameter)
 
     {
+        string ResultString;
 
-        var searcher = new DirectorySearcher("LDAP://" + user_mail.Split('\\').First().ToLower())
+        var searcher = new DirectorySearcher("LDAP://" + user_id.Split('\\').First().ToLower())
         {
-
-            Filter = "(&(objectCategory=User)(ObjectClass=person)(mail=" + user_mail.Split('\\').Last().ToLower() + "*))"
+            Filter = "(&(ObjectClass=person)(sAMAccountName=" + user_id.Split('\\').Last().ToLower() + "))"
         };
 
         var result = searcher.FindOne();
@@ -118,21 +157,23 @@ public static class ExcelFunctionCalls
             return string.Empty;
 
 
-        return result.Properties["mail"][0].ToString();
+        ResultString = result.Properties[dirParameter][0].ToString(); //to do loop all results
 
+
+        return ResultString;
     }
 
-    [ExcelFunction(Description = "email of user in a UDF Function")]
-    public static string B2_CN2mail([ExcelArgument(Name = "CN",
-                                                      Description = "CN as String to look up")]
-                                        string CN)
+
+
+    private static string CN2(string cn,
+                                string dirParameter)
 
     {
 
-        var searcher = new DirectorySearcher("LDAP://" + CN.Split('\\').First().ToLower())
+        var searcher = new DirectorySearcher("LDAP://" + cn.Split('\\').First().ToLower())
         {
 
-            Filter = "(&(objectCategory=User)(ObjectClass=person)(CN=" + CN.Split('\\').Last().ToLower() + "*))"
+            Filter = "(&(objectCategory=User)(ObjectClass=person)(CN=" + cn.Split('\\').Last().ToLower() + "*))"
         };
 
         var result = searcher.FindOne();
@@ -140,55 +181,110 @@ public static class ExcelFunctionCalls
             return string.Empty;
 
 
-        return result.Properties["mail"][0].ToString();
+        return result.Properties[dirParameter][0].ToString();
 
+    }
+
+
+
+
+
+
+    [ExcelFunction(Description = "mail of user.  (C# UDF Function")]
+    public static string B2_User2mail([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "mail").ToLower();
+    }
+
+
+    [ExcelFunction(Description = "sAMAccountName of user.  (C# UDF Function")]
+    public static string B2_User2user([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "sAMAccountName").ToLower();
+    }
+
+
+    [ExcelFunction(Description = "distinguishedName of user.  (C# UDF Function")]
+    public static string B2_User2dn([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "distinguishedName").ToLower();
+    }
+
+
+    [ExcelFunction(Description = "userPrincipalName of user.  (C# UDF Function")]
+    public static string B2_User2upn([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "userPrincipalName");
+    }
+
+
+    [ExcelFunction(Description = "CN of user.  (C# UDF Function")]
+    public static string B2_User2cn([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "CN");
+    }
+
+
+    [ExcelFunction(Description = "givenName of user.  (C# UDF Function")]
+    public static string B2_User2gn([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "givenName");
+    }
+
+    [ExcelFunction(Description = "sn / lastname of user.  (C# UDF Function")]
+    public static string B2_User2sn([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "sn");
+    }
+
+    [ExcelFunction(Description = "title of user.  (C# UDF Function")]
+    public static string B2_User2title([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "title");
+    }
+
+    [ExcelFunction(Description = "description of user.  (C# UDF Function")]
+    public static string B2_User2desc([ExcelArgument(Name = "user_id",  Description = "user id (eg johanb01)")]
+                                     string user_id)
+    {
+        return User2(user_id, "description");
+    }
+
+
+
+
+
+   // of CN Excel UDFfunctions
+
+                     
+
+
+
+    //sAMAccountName
+    [ExcelFunction(Description = "sAMAccountName of CN")]
+    public static string B2_CN2sAMAccountName([ExcelArgument(Name = "CN", Description = "CN as String to look up")]
+                                           string CN)
+    {
+        return CN2(CN, "sAMAccountName").ToLower();
     }
 
     //sAMAccountName
-    [ExcelFunction(Description = "email of user in a UDF Function")]
-    public static string B2_CN2sAMAccountName([ExcelArgument(Name = "CN",
-                                                      Description = "CN as String to look up")]
-                                        string CN)
-
+    [ExcelFunction(Description = "Email of CN")]
+    public static string B2_CN2mail([ExcelArgument(Name = "CN", Description = "CN as String to look up")]
+                                           string CN)
     {
-
-        var searcher = new DirectorySearcher("LDAP://" + CN.Split('\\').First().ToLower())
-        {
-
-            Filter = "(&(objectCategory=User)(ObjectClass=person)(CN=" + CN.Split('\\').Last().ToLower() + "*))"
-        };
-
-        var result = searcher.FindOne();
-        if (result == null)
-            return string.Empty;
-
-
-        return result.Properties["sAMAccountName"][0].ToString();
-
+        return CN2(CN, "mail").ToLower();
     }
 
-    //Location
-    //https://ldap.com/ldap-filters/
-    [ExcelFunction(Description = "Location of user in a UDF Function")]
-    public static string B2_LocationOf([ExcelArgument(Name = "UserName",
-                                                      Description = "User as String to look up location")]
-                                        string UserName)
-
-    {
-
-        var searcher = new DirectorySearcher("LDAP://" + UserName.Split('\\').First().ToLower())
-        {
-
-            Filter = "(&(objectCategory=User)(ObjectClass=person)(givenName:dn:=" + UserName.Split('\\').Last().ToLower() + "))"
-        };
-
-        var result = searcher.FindOne();
-        if (result == null)
-            return string.Empty;
 
 
-        return result.Properties["distinguishedName"][0].ToString();
-
-    }
 
 }
